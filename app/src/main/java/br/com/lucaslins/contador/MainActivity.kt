410d6f716f5ceebd96abd0a562fccb2b0f1893e0
Lucas Barbosa Lins <lucas.barbosa.lins@hotmail.com>
2025-04-03 20:24:02 -0300

    contador

---
+++ app/src/main/java/br/com/lucaslins/contador/MainActivity.kt
@@ -0,0 +1,55 @@
+package br.com.lucaslins.contador
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    //Variável do contador
+    private var contador = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+    // Início da área de funcionamento dos botões e visor
+
+        // Referêmcia ao visor e botão
+        val contadorTextView = findViewById<TextView>(R.id.contadorTextView)
+        val aumentarButton = findViewById<Button>(R.id.aumentarButton)
+        val diminuirButton = findViewById<Button>(R.id.diminuirButton)
+
+        // Função que atualiza o texto do contador
+        fun atualizarContador() {
+            contadorTextView.text = contador.toString()
+        }
+        // Botão para aumentar
+        aumentarButton.setOnClickListener {
+            contador++
+            atualizarContador()
+        }
+
+        // Botão para diminuir
+        diminuirButton.setOnClickListener {
+            if (contador > 0) { // Solicitado que o contador não fique negativo
+                contador--
+                atualizarContador()
+            }
+        }
+
+        // Atualiza o contador pelos botões de diminuir e aumentar
+        atualizarContador()
+    }
+}
